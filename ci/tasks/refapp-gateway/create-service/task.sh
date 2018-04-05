512fc1263fe135100bad186098d67b8472170061
morganstakecgi <morgan.stake@cgi.com>
2018-04-05 12:31:49 -0600

    Wait for the Service to be created before continuing

--- ci/tasks/refapp-gateway/create-service/task.sh
+++ ci/tasks/refapp-gateway/create-service/task.sh
@@ -10,6 +10,8 @@ function createServiceIfNeeded()
     else
         echo "Creating service $5"
         $*
+
+
     fi
 }
 
@@ -17,3 +19,12 @@ function createServiceIfNeeded()
 cf login --skip-ssl-validation -o ${CF_ORG} -s ${CF_SPACE} -a $CF_API -u $CF_API_USER -p $CF_API_PASSWORD
 
 createServiceIfNeeded cf create-service $REFAPP_SERVICE $REFAPP_SVC_PLAN $REFAPP_SVC_INSTANCE
+
+service_status=''
+
+while [[ "$service_status" != "succeeded" ]]
+do
+  service_status=$(cf curl /v2/service_instances/$(cf service ${REFAPP_SVC_INSTANCE} --guid)|jq -r '.entity.last_operation.state' )
+  echo $service_status
+  sleep 2
+done
