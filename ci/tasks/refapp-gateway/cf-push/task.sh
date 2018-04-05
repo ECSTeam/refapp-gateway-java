10f81d628ce89335f4290e821ab8d08b3dee2089
morganstakecgi <morgan.stake@cgi.com>
2018-04-05 09:47:50 -0600

    Changed app to use Manifest to push with the right path

--- ci/tasks/refapp-gateway/cf-push/task.sh
+++ ci/tasks/refapp-gateway/cf-push/task.sh
@@ -3,7 +3,7 @@ cd refapp-gateway-java-release
 tar -xvzf refapp-gateway-java.tar.gz
 
 cf login --skip-ssl-validation -o ${CF_ORG} -s ${CF_SPACE} -a $CF_API -u $CF_API_USER -p $CF_API_PASSWORD
-CMD="cf push ${CF_APP_NAME} -p gateway-0.0.1-SNAPSHOT.jar --route-path ${CF_ORG}-${CF_SPACE}-${CF_APP_NAME} -f manifest.yml"
+CMD="cf push ${CF_APP_NAME} -p gateway-0.0.1-SNAPSHOT.jar --route-path ${CF_ORG}-${CF_SPACE}-${CF_APP_NAME} -f ../refapp-gateway-java/manifest.yml"
 echo $CMD
 $CMD
 
